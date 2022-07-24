3b598685b2de2f84d9d1ee034633049b5b975aa5
Ankush Sharma <ankush.s@smarter.codes>
2022-07-24 20:54:00 +0530

    add: added dynamic route

---
+++ pages/products/[productId].js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+const ProductDetails = () => {
+    const router = useRouter()
+    const productId = router.query.productId
+  return (
+    <h1>Details about product {productId}</h1>
+  )
+}
+
+export default ProductDetails
