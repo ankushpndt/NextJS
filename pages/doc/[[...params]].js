adfd8f58d60567a541379a78dd3848cbe58d9fae
Ankush <ankushpndt@gmail.com>
2022-08-12 01:14:41 +0530

    feat: catchAllRoutes

---
+++ pages/doc/[[...params]].js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+const Docs = () => {
+  const router = useRouter();
+  const { params = [] } = router.query;
+  if (params.length === 2)
+    return (
+      <h1>
+        Viewing docs for feature {params[0]} and concept {params[1]}
+      </h1>
+    );
+  else if (params.length === 1)
+    return <h1>Viewing docs for feature {params[0]}</h1>;
+  return <h1>Docs HomePage</h1>;
+};
+export default Docs;
