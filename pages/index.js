104f027bc6d2f85ec0148705c0423d3bbfa7ebf9
ankush.s212 <ankush.s@smarter.codes>
2022-09-12 21:49:59 +0530

    feat: add product page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,33 @@
+import Link from "next/link";
 import React from "react";
+import { useRouter } from 'next/router';
 
 const index = () => {
-	return <h1>Home Page</h1>;
+	const router = useRouter()
+	return (
+		<div>
+			<h1>Home Page</h1>;
+			<Link href="/about">
+				<a>About</a>
+			</Link>
+			{" "}
+			<Link href="/blogs">
+				<a>Blogs</a>
+			</Link>
+			{" "}
+			<Link href="/doc">
+				<a>Docs</a>
+			</Link>
+			{" "}
+			<Link href="/products">
+				<a>Products</a>
+			</Link>
+			{" "}
+			<Link href="/profile">
+				<a>Profile</a>
+			</Link>
+			<button onClick={() => router.push("/products")}>Place order</button>
+		</div>
+	)
 };
 export default index;
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import Link from "next/link";
 
-const Products = () => {
+const Products = ({ productId = 100 }) => {
+
   return (
     <div>
         <h1>Products</h1>
-            <h1>Product 1</h1>
-            <h1>Product 2</h1>
-            <h1>Product 3</h1>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          {" "}
+          <Link href="products/1">
+            <a><h1>Product 1</h1></a>
+          </Link>
+          {" "}
+          <Link href="products/2">
+            <a><h1>Product 2</h1></a>
+          </Link>
+          {" "}
+          <Link href="products/3">
+            <a><h1>Product 3</h1></a>
+          </Link>
+          <Link href={`product/${productId}`}>
+            <a><h1>Product {productId}</h1></a>
+          </Link>
     </div>
   )
 }
