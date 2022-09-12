9901153e83437bf97d7984ec2c27ec988ccd281d
ankush.s212 <ankush.s@smarter.codes>
2022-09-12 22:51:34 +0530

    add: users button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Home = () => {
-  return <h1>Next JS pre-rendering</h1>
+  return <>
+    <h1>Next JS pre-rendering</h1>
+    <Link href="/users">
+      <a>Users</a>
+    </Link>
+  </>
 }
 
 export default Home
